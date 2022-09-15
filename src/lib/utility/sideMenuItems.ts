e5fd2d624fac27bc082f9910a6378e8325334ccb
setbap <ebr.sina@outlook.com>
2022-09-15 08:34:30 -0700

    change deep dive postion

--- src/lib/utility/sideMenuItems.ts
+++ src/lib/utility/sideMenuItems.ts
@@ -6,7 +6,6 @@ import { AiFillPicture } from "react-icons/ai";
 export default [
   { name: "Home", path: "/", icon: FiHome },
   { name: "NFTs Information", path: "/nfts_information", icon: AiFillPicture },
-  { name: "Deep Dive", path: "/all_day_deep_dive", icon: DiGoogleAnalytics },
   { name: "NFT Analysis", path: "/nft_analysis", icon: DiGoogleAnalytics },
   {
     name: "Player Analysis",
@@ -14,6 +13,7 @@ export default [
     icon: DiGoogleAnalytics,
   },
   { name: "Team Analysis", path: "/team_analysis", icon: DiGoogleAnalytics },
+  { name: "Deep Dive", path: "/all_day_deep_dive", icon: DiGoogleAnalytics },
   { name: "About", path: "/about", icon: AiOutlineInfoCircle },
 
   // { name: "TX History", path: "/transactions", icon: AiOutlineInfoCircle },
